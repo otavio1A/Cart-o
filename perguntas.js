b022f8b3b68463e40c762ab8aa43970479f58f5b
otavio1A <167330422+otavio1A@users.noreply.github.com>
2025-06-23 11:18:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o pais mais pequeno do mundo?',
+    'É o Vaticano '
+)
+
+criaCartao(
+    'História',
+    'Quem pintou a Mona Lisa?',
+    'Leonardo Da Vinci'
+)
